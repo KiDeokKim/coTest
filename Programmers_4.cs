9c33555eede7c3eaf9826c01bef59cc3d88fe401
KiDeokKim <rlarlejr7163@naver.com>
2021-04-23 16:37:34 +0900

    소수 구하기

---
+++ Programmers_4.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Programmers_4
+	{
+		/***
+		 * LV1
+		 * Programmers
+		 * 3개의 값 더해서 소수 구하기
+		 */
+		public int solution(int[] nums)
+		{
+			int count = 0;
+
+			for (int i = 0; i < nums.Length; i++)
+			{
+				for (int j = i + 1; j < nums.Length; j++)
+				{
+					for (int k = j + 1; k < nums.Length; k++)
+					{
+						int dummy = nums[i] + nums[j] + nums[k];
+						bool notPrimeNum = false;
+						for (int l = 2; l < dummy; l++)
+						{
+							if (dummy % l == 0)
+							{
+								notPrimeNum = true;
+								break;
+							}
+						}
+						if (!notPrimeNum)
+						{
+							count += 1;
+						}
+					}
+				}
+			}
+
+			return count;
+		}
+	}
+}
