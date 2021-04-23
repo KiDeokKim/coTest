7cf21ef127bb6d33ddbbaedbe676a52d74e2d56d
KiDeokKim <rlarlejr7163@naver.com>
2021-04-23 16:10:39 +0900

    20210423

---
+++ Programmers_3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Programmers_3
+	{
+		/***
+		 * LV1
+		 * 
+		 * Programmers Getdayname
+		 */
+		public string solution(int month, int day)
+		{
+			string answer = "";
+			string[] strDay = { "SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT" };
+			int[] monthDay = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+			int dayCount = 0;
+			int totDay = 0;
+			//added before month
+			for (int i = 1; i < month; i++)
+			{
+				dayCount += monthDay[i - 1];
+			}
+			//added this month
+			dayCount += (day - 1);
+			//added Friday(2016)
+			dayCount = (dayCount + 5) % 7;
+			answer = strDay[dayCount];
+			return answer;
+		}
+	}
+}
