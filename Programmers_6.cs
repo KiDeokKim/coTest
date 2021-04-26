d577a79539558720e7a55e6c5ca4a0d1468f807e
KiDeokKim <rlarlejr7163@naver.com>
2021-04-26 21:07:05 +0900

    get Correct parenthesis

---
+++ Programmers_6.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /**
+	 * LV1
+	 * Programmers 
+	 * 올바른 괄호 구하기
+	 * https://programmers.co.kr/learn/courses/30/lessons/12909
+	 * "()()"	true
+     * "(())()"	true
+     * ")()("	false
+     * "(()("	false
+	 */
+    class Programmers_6
+	{
+        public bool solution(string s)
+        {
+            bool answer = true;
+            int strLen = s.Length;
+            char[] strChr = s.ToCharArray();
+            int count = 0;
+            for (int i = 0; i < strLen; i++)
+            {
+                if (strChr[0] != '(')
+                {
+                    count = -1;
+                    break;
+                }
+                else
+                {
+                    if (strChr[i] == '(')
+                        count += 1;
+                    else
+                    {
+                        if (count <= 0)
+                        {
+                            count = -1;
+                            break;
+                        }
+                        else
+                            count -= 1;
+                    }
+
+                }
+            }
+            if (count == 0)
+                answer = true;
+            else
+                answer = false;
+            return answer;
+        }
+    }
+}
